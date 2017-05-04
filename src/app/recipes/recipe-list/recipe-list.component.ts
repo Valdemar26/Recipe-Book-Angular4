ec1c43a01aa8331d085fdcbd23c6e353504fc2f8
Valdemar26 <z_valdemar@ukr.net>
2017-05-04 15:53:44 +0300

    04May commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+      new Recipe('A Test Recipe', 'This is a simply Description',
+                 'https://cdn.pixabay.com/photo/2016/10/13/19/07/eat-1738548_960_720.jpg'),
+    new Recipe('A Test Recipe', 'This is a simply Description',
+        'https://cdn.pixabay.com/photo/2016/10/13/19/07/eat-1738548_960_720.jpg')
+  ];
 
   constructor() { }
 
