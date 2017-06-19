759479c1689aa72a71a6c737565c48cc14b57815
Valdemar26 <z_valdemar@ukr.net>
2017-06-19 14:46:10 +0300

    finished reactive forms

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs/Subject";
+
 import { Recipe } from './recipe.model';
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+
 @Injectable()
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -18,7 +22,8 @@ export class RecipeService {
         new Recipe(
             'Big Fat Burger',
             'What else you need to say?',
-            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/' +
+            'Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
             [
                 new Ingredient('Buns', 2),
                 new Ingredient('Meat', 1)
@@ -37,5 +42,20 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
